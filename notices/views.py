9a1ddca2d934cbcb7a1156325fb83ee2cee09be1
cfclindsey <lindsey@countryfriedcreative.com>
2019-07-12 11:04:35 -0400

    Template installation

---
+++ notices/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    #return HttpResponse('Welcome to GA Lobby')
+    return render(request, 'notices/base.html')
